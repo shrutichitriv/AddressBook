abca2dbd9b872649bbcbd59cd20e3dcdfb3c5036
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-11 22:19:25 +0530

    [Shruti] Add a new contact in address book

--- AddressBook_244/Program.cs
+++ AddressBook_244/Program.cs
@@ -7,10 +7,59 @@
 
             Console.WriteLine("Welcome To Address Book Problem");
 
+            //AdressBookBuilder adressBookBuilder = new AdressBookBuilder();
+            //adressBookBuilder.AddAdressBook("Ram", "Sharma", "Sadar", "Nagpur", "Maharashtra", "440009", "7973455678", "Ram199@gmail.com");
+            //adressBookBuilder.DisplayAddressBook();
+
             AdressBookBuilder adressBookBuilder = new AdressBookBuilder();
-            adressBookBuilder.AddAdressBook("Ram", "Sharma", "Sadar", "Nagpur", "Maharashtra", "440009", "7973455678", "Ram199@gmail.com");
+
+            bool check = true;
+            while (check)
+            {
+
+                Console.WriteLine("Enter First Name");
+                string firstName = Console.ReadLine();
+
+                Console.WriteLine("Enter Last Name");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter Address");
+                string address = Console.ReadLine();
+
+                Console.WriteLine("Enter City Name");
+                string city = Console.ReadLine();
+
+
+                Console.WriteLine("Enter State Name");
+                string state = Console.ReadLine();
+
+                Console.WriteLine("Enter  Zip number");
+                string zip = Console.ReadLine();
+
+                Console.WriteLine("Enter Phone Number");
+                string pnoneNumber = Console.ReadLine();
+
+                Console.WriteLine("Enter Email");
+                string email = Console.ReadLine();
+
+                adressBookBuilder.AddAdressBook(firstName, lastName, address, city, state, zip, pnoneNumber, email);
+
+                Console.WriteLine("If you want to add another Enter Y ");
+                string addOrNot = Console.ReadLine().ToLower();
+
+                if (addOrNot == "y")
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                }
+            }
             adressBookBuilder.DisplayAddressBook();
+
+
         }
-            
+
     }
 }
