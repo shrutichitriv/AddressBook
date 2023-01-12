ac4aead3d52528a1f137c44d1b64f5e109c29212
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-12 16:52:18 +0530

    [Shruti] To add multiple person to address book

--- AddressBook_244/AdressBookBuilder.cs
+++ AddressBook_244/AdressBookBuilder.cs
@@ -94,7 +94,5 @@ namespace AddressBook_244
                 }
             }
         }
-
-
     }
 }
--- AddressBook_244/Program.cs
+++ AddressBook_244/Program.cs
@@ -11,61 +11,61 @@
             //adressBookBuilder.AddAdressBook("Ram", "Sharma", "Sadar", "Nagpur", "Maharashtra", "440009", "7973455678", "Ram199@gmail.com");
             //adressBookBuilder.DisplayAddressBook();
 
-            //    AdressBookBuilder adressBookBuilder = new AdressBookBuilder();
-
-            //    bool check = true;
-            //    while (check)
-            //    {
-
-            //        Console.WriteLine("Enter First Name");
-            //        string firstName = Console.ReadLine();
-
-            //        Console.WriteLine("Enter Last Name");
-            //        string lastName = Console.ReadLine();
-
-            //        Console.WriteLine("Enter Address");
-            //        string address = Console.ReadLine();
-
-            //        Console.WriteLine("Enter City Name");
-            //        string city = Console.ReadLine();
-
-
-            //        Console.WriteLine("Enter State Name");
-            //        string state = Console.ReadLine();
-
-            //        Console.WriteLine("Enter  Zip number");
-            //        string zip = Console.ReadLine();
-
-            //        Console.WriteLine("Enter Phone Number");
-            //        string pnoneNumber = Console.ReadLine();
-
-            //        Console.WriteLine("Enter Email");
-            //        string email = Console.ReadLine();
-
-            //        adressBookBuilder.AddAdressBook(firstName, lastName, address, city, state, zip, pnoneNumber, email);
-
-            //        Console.WriteLine("If you want to add another Enter Y ");
-            //        string addOrNot = Console.ReadLine().ToLower();
-
-            //        if (addOrNot == "y")
-            //        {
-            //            check = true;
-            //        }
-            //        else
-            //        {
-            //            check = false;
-            //        }
-            //    }
-            //    adressBookBuilder.DisplayAddressBook();
-
-
             AdressBookBuilder adressBookBuilder = new AdressBookBuilder();
 
-            adressBookBuilder.AddAdressBook("Amit", "Roy", "Ram nagar", "Dknl", "Kolkata", "759022", "7978861488", "amit@gmail.com");
-            adressBookBuilder.AddAdressBook("Raj", "Kumar", "Jatpura", "Fshh", "Raipur", "759023", "7978861434", "raj@gmail.com");
-            adressBookBuilder.AddAdressBook("Mamta", "Shah", "Gandhibagh", "Ghji", "Kolhapur", "759024", "7978861484", "mamta@gmail.com");
-            adressBookBuilder.AddAdressBook("Mayank", "Kapoor", "Nagar", "Dfgh", "Satara", "759025", "7978861485", "mayank@gmail.com");
+            bool check = true;
+            while (check)
+            {
+
+                Console.WriteLine("Enter First Name");
+                string firstName = Console.ReadLine();
+
+                Console.WriteLine("Enter Last Name");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter Address");
+                string address = Console.ReadLine();
+
+                Console.WriteLine("Enter City Name");
+                string city = Console.ReadLine();
+
+
+                Console.WriteLine("Enter State Name");
+                string state = Console.ReadLine();
+
+                Console.WriteLine("Enter  Zip number");
+                string zip = Console.ReadLine();
+
+                Console.WriteLine("Enter Phone Number");
+                string pnoneNumber = Console.ReadLine();
+
+                Console.WriteLine("Enter Email");
+                string email = Console.ReadLine();
+
+                adressBookBuilder.AddAdressBook(firstName, lastName, address, city, state, zip, pnoneNumber, email);
+
+                Console.WriteLine("If you want to add another Enter Y ");
+                string addOrNot = Console.ReadLine().ToLower();
+
+                if (addOrNot == "y")
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                }
+            }
             adressBookBuilder.DisplayAddressBook();
+
+
+            //AdressBookBuilder adressBookBuilder = new AdressBookBuilder();
+
+            //adressBookBuilder.AddAdressBook("Amit", "Roy", "Ram nagar", "Dknl", "Kolkata", "759022", "7978861488", "amit@gmail.com");
+            //adressBookBuilder.AddAdressBook("Raj", "Kumar", "Jatpura", "Fshh", "Raipur", "759023", "7978861434", "raj@gmail.com");
+            //adressBookBuilder.AddAdressBook("Mamta", "Shah", "Gandhibagh", "Ghji", "Kolhapur", "759024", "7978861484", "mamta@gmail.com");
+            //adressBookBuilder.AddAdressBook("Mayank", "Kapoor", "Nagar", "Dfgh", "Satara", "759025", "7978861485", "mayank@gmail.com");
+            //adressBookBuilder.DisplayAddressBook();
 
 
             ////For Editiang A Person Contact Details
